39910e91332edea2f29512f3bfb8417ee1c8dcac
Sion Kang <siontama@gmail.com>
2023-06-08 22:18:31 +0900

    auto scaling group

--- vpc.tf
+++ vpc.tf
@@ -84,51 +84,56 @@ resource "aws_security_group" "security_group" {
   provider = "aws.${local.regions[count.index]}"
 }
 
-//Instance
-resource "aws_instance" "public_instance" {
-    count = 3
-    ami           = "ami-0c94855ba95c71c99"
-    instance_type = "t2.micro"
-    subnet_id     = aws_subnet.subnet[count.index * 2].id
-    tags = {
-        Name = "public-instance-${count.index}"
-    }
-    provider = aws.regions[count.index]
+resource "aws_launch_configuration" "launch_configuration" {
+  count = 3
+  name_prefix = "launch-configuration-${count.index}"
+  image_id = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.security_group[count.index].id]
+  provider = "aws.${local.regions[count.index]}"
 }
 
-//ALB
+resource "aws_autoscaling_group" "autoscaling_group" {
+  name = "autoscaling-group"
+  launch_configuration = aws_launch_configuration.launch_configuration[0].name
+  min_size = 1
+  max_size = 3
+  desired_capacity = 2
+  vpc_zone_identifier = [aws_subnet.subnet[0].id, aws_subnet.subnet[2].id, aws_subnet.subnet[4].id]
+  target_group_arns = [aws_lb_target_group.target_group[0].arn]
+  provider = "aws.${local.regions[0]}"
+}
+
 resource "aws_lb" "lb" {
   name = "my-lb"
-  subnets = [aws_subnet.subnet[0].id, aws_subnet.subnet[2].id, aws_subnet.subnet[4].id]
+  internal = false
+  load_balancer_type = "application"
+  subnets = [aws_subnet.subnet[1].id, aws_subnet.subnet[3].id, aws_subnet.subnet[5].id]
   security_groups = [aws_security_group.security_group[0].id]
   provider = "aws.${local.regions[0]}"
 }
 
 resource "aws_lb_target_group" "target_group" {
-  count = 3
-  name = "my-target-group-${count.index}"
+  name = "my-target-group"
   port = 80
   protocol = "HTTP"
-  vpc_id = aws_vpc.vpc[count.index].id
-  provider = "aws.${local.regions[count.index]}"
+  vpc_id = aws_vpc.vpc[0].id
+  provider = "aws.${local.regions[0]}"
 }
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.lb.arn
   port = 80
   protocol = "HTTP"
-
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.target_group[0].arn
+    target_group_arn = aws_lb_target_group.target_group.arn
   }
   provider = "aws.${local.regions[0]}"
 }
 
-resource "aws_lb_target_group_attachment" "target_group_attachment" {
-  count = 3
-  target_group_arn = aws_lb_target_group.target_group[count.index].arn
-  target_id = aws_instance.instance[count.index].id
-  port = 80
-  provider = "aws.${local.regions[count.index]}"
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.autoscaling_group.name
+  alb_target_group_arn = aws_lb_target_group.target_group.arn
+  provider = "aws.${local.regions[0]}"
 }
