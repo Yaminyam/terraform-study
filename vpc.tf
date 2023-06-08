d529757c0db14c775f86d41302054b52e25a3b29
Sion Kang <siontama@gmail.com>
2023-06-08 22:12:03 +0900

    alb, instance

--- vpc.tf
+++ vpc.tf
@@ -62,3 +62,52 @@ resource "aws_ec2_transit_gateway_attachment" "attachment" {
     transit_gateway_id = aws_ec2_transit_gateway.transit_gateway.id
     vpc_id = aws_vpc.vpc[count.index].id
 }
+
+//Instance
+resource "aws_instance" "public_instance" {
+    count = 3
+    ami           = "ami-0c94855ba95c71c99"
+    instance_type = "t2.micro"
+    subnet_id     = aws_subnet.subnet[count.index * 2].id
+    tags = {
+        Name = "public-instance-${count.index}"
+    }
+    provider = aws.regions[count.index]
+}
+
+//ALB
+resource "aws_lb" "lb" {
+  name = "my-lb"
+  subnets = [aws_subnet.subnet[0].id, aws_subnet.subnet[2].id, aws_subnet.subnet[4].id]
+  security_groups = [aws_security_group.security_group[0].id]
+  provider = "aws.${local.regions[0]}"
+}
+
+resource "aws_lb_target_group" "target_group" {
+  count = 3
+  name = "my-target-group-${count.index}"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpc[count.index].id
+  provider = "aws.${local.regions[count.index]}"
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target_group[0].arn
+  }
+  provider = "aws.${local.regions[0]}"
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment" {
+  count = 3
+  target_group_arn = aws_lb_target_group.target_group[count.index].arn
+  target_id = aws_instance.instance[count.index].id
+  port = 80
+  provider = "aws.${local.regions[count.index]}"
+}
